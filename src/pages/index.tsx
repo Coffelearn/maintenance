ad3250f1bd6945b46027d615c4dd5d841e7d9a60
JesseAvilaa <jesseavila2002@gmail.com>
2021-09-10 15:22:26 -0300

    maintenance | 0.1.0

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/Head';
+
+import styles from './home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Página em manutenção</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Olá, bem-vindo.</span>
+          <h1>
+            Está página está em <span>manutenção</span>.
+          </h1>
+          <p>Em breve retornamos...</p>
+        </section>
+        <img className={styles.image} src="/images/home.svg" />
+      </main>
+    </>
+  );
+}
